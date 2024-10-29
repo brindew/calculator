2d88b7a3e67d494803b8e3ead3614e311f309449
brindew <brindewsingh2002@gmail.com>
2024-10-29 12:37:35 +0530

    first commit

---
+++ index_loops.js
@@ -0,0 +1,26 @@
+let a = 1;
+
+for (let i = 0; i < 100; i++) {
+    console.log(a+i); 
+}
+
+let obj = {
+    name : "Brindew",
+    role : "Programmer",
+    company : "Coding"
+}
+
+let i = 1;
+
+while (i < 6){
+    console.log(i)
+    i++;
+}
+
+dowhile loop
+let i = 0
+
+do{
+    console.log(i)
+    i++;
+}while(i<6);
