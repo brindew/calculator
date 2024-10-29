2dfd7242c3bac5f04c7cd67045403ed03e8a490b
brindew <brindewsingh2002@gmail.com>
2024-10-29 12:35:11 +0530

    fix

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello Brindew this side")
+
+let age =  20;
+let grace = 2;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if(age>21){
+    console.log("You can drive");
+}
+
+else if(age == 20){
+    console.log("You can drive but slowlt")
+}
+
+else{
+    console.log("You cannot drive");
+}
